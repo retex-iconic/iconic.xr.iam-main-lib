b8aaf891c604dff453a48e5ff948c6f77b8f5d3c
RETEX\l.garcia <lilia.garcia@retex.com>
2024-07-25 10:25:18 +0200

    feat: #IAM-237

--- src/main/java/com/retexspa/xr/ms/iam/main/query/repositories/AnagAttributiRuoliRepository.java
+++ src/main/java/com/retexspa/xr/ms/iam/main/query/repositories/AnagAttributiRuoliRepository.java
@@ -1,9 +1,15 @@
 package com.retexspa.xr.ms.iam.main.query.repositories;
 
-import com.retexspa.xr.ms.iam.main.query.entities.AnagAttributiRuoliQueryEntity;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface AnagAttributiRuoliRepository extends JpaRepository<AnagAttributiRuoliQueryEntity, String>, JpaSpecificationExecutor<AnagAttributiRuoliQueryEntity> {
+import com.retexspa.xr.ms.iam.main.query.entities.AnagAttributiRuoliQueryEntity;
+
+public interface AnagAttributiRuoliRepository extends JpaRepository<AnagAttributiRuoliQueryEntity, String>,
+        JpaSpecificationExecutor<AnagAttributiRuoliQueryEntity> {
+
+    List<AnagAttributiRuoliQueryEntity> findAllByApplicazioneId(String id);
 
 }
